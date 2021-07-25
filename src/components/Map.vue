ed56a6e0c15fc1e24e42ca32488c613b397f701c
jraihan-me <jahirraihanmsd@gmail.com>
2021-07-25 19:43:30 +0600

    initial commit

---
+++ src/components/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Your coordinates</h1>
+    <p>{{ coordinates.lat }} latitude, {{ coordinates.lng }} longitude</p>
+    <GmapMap
+      :center="{ lat: 10, lng: 10 }"
+      :zoom="7"
+      style="width: 640px; height: 360px"
+    >
+    </GmapMap>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      coordinates: {
+        lat: 0,
+        lng: 0,
+      },
+    };
+  },
+  created() {
+    this.$getLocation({})
+      .then((coordinates) => {
+        this.coordinates = coordinates;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import VueGeolocatoin from "vue-browser-geolocation";
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.config.productionTip = false;
+Vue.use(VueGeolocatoin);
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCBXk-KNu7-FN7Rv4i4Aege0iIuqBpZOYQ",
+  },
+  installComponents: false,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
